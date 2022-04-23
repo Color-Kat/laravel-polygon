0cc4dbf133762b8d4ac229f52093c45382180cd1
Color-Kat <katik7979@gmail.com>
2022-04-23 20:30:18 +0300

    Blog categories seeder

---
+++ database/seeders/BlogCategoriesTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BlogCategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [];
+
+        $categoryName = "Без категории";
+
+        $categories[] = [
+            "title" => $categoryName,
+            "slug" => \Str::slug($categoryName),
+            "parent_id" => 0
+        ];
+
+        for ($i=0; $i<= 10; $i++) {
+            $categoryName = "Категория #$i";
+            $parentId = $i > 5 ? rand(1, 4) : 1;
+
+            $categories[] = [
+                "title" => $categoryName,
+                "slug" => \Str::slug($categoryName),
+                "parent_id" => $parentId
+            ];
+        }
+
+        \DB::table('blog_categories')->insert($categories);
+    }
+}
