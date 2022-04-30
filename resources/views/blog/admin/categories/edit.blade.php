c05d6e40cebe4c80c7f68d6374a74e2a73365e7c
Color-Kat <katik7979@gmail.com>
2022-04-30 20:39:37 +0300

    success alert

--- resources/views/blog/admin/categories/edit.blade.php
+++ resources/views/blog/admin/categories/edit.blade.php
@@ -16,10 +16,23 @@
                 <div class="justify-content-center row">
                     <div class="col-md-11">
                         <div class="alert alert-danger" role="alert">
-                            <button class="close" data-dismiss="alert" aria-label="close">
-                                <span aria-hidden="true">x</span>
-                            </button>
+{{--                            <button class="close" data-dismiss="alert" aria-label="close">--}}
+{{--                                <span aria-hidden="true">x</span>--}}
+{{--                            </button>--}}
                             {{$errors->first()}}
+                        </div>
+                    </div>
+                </div>
+            @endif
+
+            @if(session('success'))
+                <div class="justify-content-center row">
+                    <div class="col-md-11">
+                        <div class="alert alert-success" role="alert">
+{{--                            <button class="close" data-dismiss="alert" aria-label="close">--}}
+{{--                                <span aria-hidden="true">x</span>--}}
+{{--                            </button>--}}
+                            {{session()->get('success')}}
                         </div>
                     </div>
                 </div>
