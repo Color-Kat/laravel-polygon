dde3d6ef1fb6a36ef69af8bd69feb26e46fe633c
Color-Kat <katik7979@gmail.com>
2022-04-30 16:13:44 +0300

    Update category method

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -11,14 +11,12 @@ class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
         $paginator = BlogCategory::paginate(5);
 
-        return view('blog.admin.category.index', compact('paginator'));
+        return view('blog.admin.categories.index', compact('paginator'));
     }
 
     /**
@@ -35,7 +33,6 @@ class CategoryController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
@@ -46,14 +43,13 @@ class CategoryController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
         $item = BlogCategory::findOrFail($id);
         $categoryList = BlogCategory::all();
 
-        return view('blog.admin.category.edit', compact('item', 'categoryList'));
+        return view('blog.admin.categories.edit', compact('item', 'categoryList'));
     }
 
     /**
@@ -61,10 +57,27 @@ class CategoryController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        dd(__METHOD__, $request->all(), $id);
+        $item = BlogCategory::find($id);
+        if(empty($item)){
+            return back()
+                ->withErrors(['message' => "Запись с id=$id не найдена"])
+                ->withInput();
+        }
+
+        $data = $request->all();
+        $result = $item->fill($data)->save();
+
+        if($result) {
+            return redirect()
+                ->route('blog.admin.categories.edit', $id)
+                ->with(['success' => 'Успешно сохранено']);
+        } else {
+            return back()
+                ->withErrors(['message' => "Произошла ошибка при сохранении данных"])
+                ->withInput();
+        }
     }
 }
---
+++ resources/views/blog/admin/categories/includes/item_edit_main_col.blade.php
@@ -0,0 +1,79 @@
+@php
+    /** @var \App\Models\BlogCategory $item */
+    /** @var \Illuminate\Database\Eloquent\Collection $categoryList */
+@endphp
+<div class="justify-content-center row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="card-title"></div>
+
+                <ul class="nav nav-tabs">
+                    <li class="nav-item">
+                        <a href="#maindata" data-toggle="tab" role="tab" class="nav-link active">Основные данные</a>
+                    </li>
+                </ul>
+
+                <br>
+
+                <div class="tab-content">
+                    <div class="tab-pane active" id="maindata" role="tabpanel">
+                        <div class="form-group">
+                            <lavel for="title">Заголовок</lavel>
+                            <input
+                                name="title"
+                                value="{{old('title', $item->title)}}"
+                                id="title"
+                                class="form-control"
+                                minlength="3"
+                                required
+                                type="text"
+                            >
+                        </div>
+
+                        <div class="form-group">
+                            <lavel for="slug">Индетификатор</lavel>
+                            <input
+                                name="slug"
+                                value="{{old('slug', $item->slug)}}"
+                                id="slug"
+                                class="form-control"
+                                type="text"
+                            >
+                        </div>
+
+                        <div class="form-group">
+                            <lavel for="parent_id">Родитель</lavel>
+                            <select
+                                name="parent_id"
+                                id="parent_id"
+                                class="form-control"
+                            >
+                                @foreach($categoryList as $categoryOption)
+                                    <option value="{{$categoryOption->id}}"
+                                            @if($categoryOption->id == $item->parent_id) selected @endif
+                                    >
+                                        {{$categoryOption->id}}. {{$categoryOption->title}}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <lavel for="description">Описание</lavel>
+
+                            {{--     Take old insered data by withInput() in controller@update     --}}
+                            <textarea
+                                name="description"
+                                id="description"
+                                class="form-control"
+                                type="text"
+                                rows="3"
+                            >{{old('description', $item->description)}}</textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
